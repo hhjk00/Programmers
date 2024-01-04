89341bc7c211d08c7ec36010c9bf0e88cfe02e39
Hyejin Kim <97223653+hhjk00@users.noreply.github.com>
2024-01-04 15:40:05 +0900

    [level unrated] Title: 카운트 업, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,11 @@
+function solution(start_num, end_num) {
+    var answer = [];
+    let num = start_num
+    
+    while (num <= end_num) {
+        answer.push(num)
+        num++
+    }
+        
+    return answer;
+}
