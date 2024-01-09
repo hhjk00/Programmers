abfcca7a7d360eaec5d858bb1db8e055f8c4d266
Hyejin Kim <97223653+hhjk00@users.noreply.github.com>
2024-01-09 23:14:33 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,10 +1,15 @@
 function solution(my_string, is_suffix) {
-    let i = 0
-    let my_suffix = []
+//  1. 배열로 만들어서 확인
+//     let i = 0
+//     let my_suffix = []
     
-    while(i < my_string.length) {
-        my_suffix.push(my_string.substring(my_string.length - ++i))
-    }
+//     while(i < my_string.length) {
+//         my_suffix.push(my_string.substring(my_string.length - ++i))
+//     }
     
-    return my_suffix.includes(is_suffix) ? 1 : 0
+//     return my_suffix.includes(is_suffix) ? 1 : 0
+    
+// 2. endsWith() 활용
+    return my_string.endsWith(is_suffix) ? 1 : 0
+    
 }
