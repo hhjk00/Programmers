6d83b61b7eec86d2d409d659b476eb6de3e24970
Hyejin Kim <97223653+hhjk00@users.noreply.github.com>
2024-01-16 00:45:56 +0900

    [level Lv.0] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/Lv.0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,7 +1,3 @@
 function solution(arr, idx) {
-    var answer = 0;
-    
-    answer = arr.findIndex((i,index) => idx <= index && i)
-    
-    return answer;
+   return arr.findIndex((i,index) => idx <= index && i)
 }
