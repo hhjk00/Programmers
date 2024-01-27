3afbcbc2c74e22fac7964b0363eae548c6d50c1e
Hyejin Kim <97223653+hhjk00@users.noreply.github.com>
2024-01-27 23:27:46 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -3,7 +3,6 @@ function solution(arr) {
     let before = [...arr]
     
     for(i=0; i<before.length; i++) {
-        let val = before[i]
         let after = before.map(v => v>=50 && !(v%2) ? v/2 : v<50 && v%2 ? v*2+1 : v)
         
         if(after.every((v,i) => before[i] === v)) return answer;
