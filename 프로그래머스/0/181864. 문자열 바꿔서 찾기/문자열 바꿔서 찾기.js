43c13055b7563d139f5f4793a409316477c79305
Hyejin Kim <97223653+hhjk00@users.noreply.github.com>
2024-02-07 21:52:11 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,7 +1,6 @@
 function solution(myString, pat) {
-    return Array.from(myString)
+    return +Array.from(myString)
         .map(v => v === 'A' ? v.replace('A', 'B') : v.replace('B', 'A'))
         .join('')
-        .includes(pat) 
-        ? 1 : 0
+        .includes(pat)
 }
