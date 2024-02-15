68e5d5f63385d831081ce142abb3c9eca8666e2c
Hyejin Kim <97223653+hhjk00@users.noreply.github.com>
2024-02-15 13:45:39 +0900

    [level 0] Title: 배열 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,15 @@
+function solution(arr1, arr2) {
+    var answer = 0;
+    
+    if(arr1.length > arr2.length ) return 1;
+    else if(arr1.length < arr2.length) return -1;
+    else if(arr1.length === arr2.length) {
+        let a1 = arr1.reduce((arr,cur) => arr+cur ,0)
+        let a2 = arr2.reduce((arr,cur) => arr+cur ,0)
+        
+        if(a1 > a2) return 1;
+        else if(a1 < a2) return -1;
+        else return 0;
+    }
+    
+}
